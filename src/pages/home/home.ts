2b73be1e786c810a99e18f65cb41d00efba28669
Alex Andreev <alexckua@active-bridge.com>
2018-05-07 02:55:33 +0300

    Implement weather functionality

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,41 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { Observable } from 'rxjs/Observable';
+import { WeatherProvider } from '../../providers/weather/weather';
+import { Storage } from '@ionic/storage';
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
+  weather:any;
+  location:{
+    city:string;
+    state:string;
+  }
 
-  constructor(public navCtrl: NavController) {
+  constructor(
+    public navCtrl: NavController,
+    private weatherProvider:WeatherProvider,
+    private storage:Storage
+  ) {
 
   }
 
+  ionViewWillEnter(){
+    this.storage.get('location').then((val) => {
+      if(val != null){
+        this.location = JSON.parse(val);
+      }else{
+        this.location = {
+          city: 'Kyiv',
+          state: 'UA'
+        }
+      }
+      this.weatherProvider.getWeather(this.location.city, this.location.state).subscribe(weather => {
+        this.weather = weather.current_observation;
+      })
+    })
+  }
 }
---
+++ src/providers/weather/weather.ts
@@ -0,0 +1,18 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import 'rxjs/add/operator/map';
+
+@Injectable()
+export class WeatherProvider {
+  apiKey = 'ae092991016884b1';
+  url;
+
+  constructor(public http: HttpClient) {
+    console.log('Hello WeatherProvider Provider');
+    this.url = 'http://api.wunderground.com/api/'+this.apiKey+'/conditions/q';
+  }
+
+  getWeather(city, state){
+    return this.http.get(this.url+'/'+state+'/'+city+'.json');
+  }
+}
